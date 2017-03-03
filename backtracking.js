4574120023eea4cd9abad6e7f1119a36502ef43f
abner <abner.ribeiro@tegra.me>
2017-03-03 20:53:15 -0300

    initial commit

---
+++ backtracking.js
@@ -0,0 +1,139 @@
+const readline = require('readline');
+const SUDOKU_BOARD_SIZE = 9;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.once('line', (count) => {
+  let board = [];
+
+  rl.on('line', (input) => {
+    board.push(input.toString().trim().split(""));
+    if (board.length === SUDOKU_BOARD_SIZE) {
+      const result = backtracking(board);
+      if (result) {
+        printBoard(result);
+      }
+      board = [];
+    }
+  });
+});
+
+function backtracking(board) {
+  if (reject(board)) {
+    return false;
+  }
+  if (accept(board)) {
+    return board;
+  }
+  for (const candidate of generator(board)) {
+    const solution = backtracking(candidate);
+    if (solution) {
+      return solution;
+    }
+  }
+  return false;
+}
+
+function* generator(board) {
+  const lineIndex = board.findIndex((line) => {
+    return line.some((number) => number === "0");
+  });
+  const colIndex = board[lineIndex].findIndex((number) => number === "0");
+
+  const solutions = generate(board, lineIndex, colIndex);
+  for (const solution of solutions) {
+      yield solution;
+  }
+}
+
+function generate(board, lineIndex, colIndex) {
+  const solutions = [];
+  const candidates = findCandidates(board, lineIndex, colIndex);
+  candidates.forEach((candidate) => {
+    const solution = board.slice();
+    solution[lineIndex] = solution[lineIndex].slice();
+    solution[lineIndex][colIndex] = candidate;
+    solutions.push(solution);
+  });
+  return solutions;
+}
+
+const candidates = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function findCandidates(board, lineIndex, colIndex) {
+  const currentNumbers = board[lineIndex].filter((el, index) => index !== colIndex && el !== "0")
+    .concat(board.filter((line, index) => lineIndex !== index)
+      .map((line) => line[colIndex])
+      .filter(number => number !== "0")
+    );
+  return candidates;//.filter((number) => currentNumbers.includes(number));
+}
+
+function printBoard(board) {
+  board.forEach((line) => console.log(line.join('')));
+}
+
+function isDuplicated(number, index, line) {
+  return number !== '0' && line.lastIndexOf(number) !== index;
+}
+
+function reject(solution) {
+  return solution.some((line) => {
+    return line.some(isDuplicated);
+  }) || rotate(solution).some((line) => {
+    return line.some(isDuplicated);
+  }) || blocks(solution).some((line) => {
+    return line.some(isDuplicated);
+  }) ;
+}
+
+function accept(solution) {
+  return !solution.some((line) => {
+    return line.some((number) => number === '0');
+  });
+}
+
+function rotate(board) {
+  const reverseBoard = [];
+  board.forEach((line) => {
+      line.forEach((number, index) => {
+        reverseBoard[index] = reverseBoard[index] || [];
+        reverseBoard[index].push(number);
+      });
+  });
+  return reverseBoard;
+}
+
+/* Converts from
+|A|A|B|B|C|C|
+|A|A|B|B|C|C|
+...
+|G|G|H|H|I|I|
+|G|G|H|H|I|I|
+to
+|A|A|A|A|
+|B|B|B|B|
+...
+|I|I|I|I|
+*/
+function blocks(board) {
+  const blocksBoard = [];
+  board.forEach((line, lineIndex) => {
+      line.forEach((number, colIndex) => {
+        const index = Math.floor((lineIndex) / 3) * 3 + (Math.floor((colIndex) / 3));
+        blocksBoard[index] = blocksBoard[index] || [];
+        blocksBoard[index].push(number);
+      });
+  });
+  return blocksBoard;
+}
+
+module.exports = {
+  backtracking,
+  rotate,
+  blocks,
+};
---
+++ test/backtracking.spec.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const backtracking = require('../backtracking');
+
+describe('backtracking', function() {
+  describe('backtracing', function() {
+    it('should solve the sudoku problem', function() {
+      const board = [
+        "103000509".split(""),
+        "002109400".split(""),
+        "000704000".split(""),
+        "300502006".split(""),
+        "060000050".split(""),
+        "700803004".split(""),
+        "000401000".split(""),
+        "009205800".split(""),
+        "804000107".split(""),
+      ];
+      const expectedResponse = [
+        "143628579".split(""),
+        "572139468".split(""),
+        "986754231".split(""),
+        "391542786".split(""),
+        "468917352".split(""),
+        "725863914".split(""),
+        "237481695".split(""),
+        "619275843".split(""),
+        "854396127".split(""),
+      ];
+      const response = backtracking.backtracking(board);
+
+      assert.deepEqual(response, expectedResponse);
+    });
+  });
+});
